8cd44f2c509e605ec673ffa24a72d15f7bdadbf6
Divyansh Gautam <divyanshgautam0103@gmail.com>
2026-03-05 16:35:31 +0530

    Fix: add isMetaMask to Window type definition

--- global.d.ts
+++ global.d.ts
@@ -1,7 +1,8 @@
 interface Window {
-    ethereum?: {
-      request: (args: { method: string; params?: any[] }) => Promise<any>
-      on: (event: string, handler: (...args: any[]) => void) => void
-      removeListener: (event: string, handler: (...args: any[]) => void) => void
-    }
+  ethereum?: {
+    isMetaMask?: boolean
+    request: (args: { method: string; params?: any[] }) => Promise<any>
+    on: (event: string, handler: (...args: any[]) => void) => void
+    removeListener: (event: string, handler: (...args: any[]) => void) => void
   }
+}
